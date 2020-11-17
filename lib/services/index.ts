10630e2fbdc59f4eeaf353f063ff2cf55ead0b1c
vishwa.bc <vishwanath@zendynamix.com>
2020-11-17 22:00:24 +0530

    updated files

--- lib/services/index.ts
+++ lib/services/index.ts
@@ -1,9 +1,7 @@
-//import Mp4Segmenter = require("./modules/Mp4Segmenter");
 import  Mp4Segmenter  from "./modules/Mp4Segmenter"
 import {FfmpegUtilStreamer} from "./ffmpegStreamer";
 import * as Mp4Frag from "mp4frag";
 import * as FfmpegRespawn from "ffmpeg-respawn";
-//import {SocketIo} from "./socket";
 import * as streamService from "./streamer.service"
 export {FfmpegUtilStreamer};
 export {Mp4Segmenter};
